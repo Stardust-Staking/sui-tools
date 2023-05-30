da3720081875f6025e1cc4d85446ddadc0aa1463
AngelBlock <angelblock@starduststaking.com>
2023-05-30 15:06:10 +0100

    updated reward script

--- scripts/rewards/withdraw_and_merge.sh
+++ scripts/rewards/withdraw_and_merge.sh
@@ -5,14 +5,26 @@ source ./.env
 rpc="${RPC}"
 address="${ADDRESS}"
 gas_object_to_pay="${GAS_OBJECT_TO_PAY}"
+primary_coin="${PRIMARY_COIN}"
 
-# Run the curl command and get the list of ids
+# Get address's SuiStaked objects
 ids=$(curl -s -X POST -H "Content-Type: application/json" --data "{ \"jsonrpc\":\"2.0\", \"method\":\"suix_getStakes\",\"id\":1, \"params\": [\"$address\"]}" $rpc | jq -r '.result[].stakes[].stakedSuiId')
 
-# Iterate over the IDs and run your command
+# Iterate over the IDs and run sui request_withdraw_stake
+echo "Withdrawing all sui::SuiStaked objects"
+
 for id in $ids; do
     echo $id
     sui client call --package 0x3 --module sui_system --function request_withdraw_stake --args 0x5 $id --gas-budget=14000000 --gas $gas_object_to_pay > /dev/null 2>&1
     sleep 3
 done
 
+# Get address's sui::SUI objects, exclude GAS_OBJECT_TO_PAY and PRIMARY_COIN
+echo "Merging all sui::SUI objects to PRIMARY_COIN"
+coin_ids=$(curl -s -X POST -H 'Content-Type: application/json' --data "{\"jsonrpc\":\"2.0\", \"id\":1,\"method\":\"suix_getCoins\",\"params\":{\"owner\":\"$address\"},\"coin_type\":\"0x2::sui::SUI\"}" $rpc | jq -r '.result.data[].coinObjectId' | egrep -v "$gas_object_to_pay|$primary_coin")
+
+for id in $coin_ids; do
+    echo $id
+    sui client merge-coin --primary-coin $primary_coin --coin-to-merge $id  --gas-budget=2000000 --gas $gas_object_to_pay > /dev/null 2>&1
+    sleep 3
+done
